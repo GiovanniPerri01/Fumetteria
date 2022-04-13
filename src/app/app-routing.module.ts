8d319bd9d4377358aafcb68de1485075d2090f05
Giovanni <gperri39@gmail.com>
2022-04-13 15:39:39 +0200

    aggiunta pagina di registrazione(temporanea, da modificare griglia)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
 ];
 
 @NgModule({
