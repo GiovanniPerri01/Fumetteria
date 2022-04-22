eabe15e711337642f7e4885b2e2583a040145bcf
Giovanni <gperri39@gmail.com>
2022-04-22 12:03:37 +0200

    Aggiunto ts per mostrare username e password alla pressione del bottone tramite un alert

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -5,13 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
+
+  username:string;
+  password:string;
 
   constructor() { }
 
+  loginClick():void{
+    alert("Username: " + this.username + "    " +"Password: " + this.password);
+  }
+
   ngOnInit() {
   }
-  login(){
-    console.log("sono nella login");
-  }
+
 }
